feff276c74b0e71d9d88e2ea751e4a6d989294c8
leaven153 <busmoja@naver.com>
2024-01-31 21:55:43 +0900

    240131_intelliJ(leaven153)_ICT_setter 추가중...

--- src/main/java/com/leaven/mmom/entity/MmomProject.java
+++ src/main/java/com/leaven/mmom/entity/MmomProject.java
@@ -6,12 +6,13 @@ import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 
 /**********
  * class: 생성된 프로젝트에 대한 정보
  * field: id, 생성자id, 프로젝트타입(solo-private, solo-shared, team), 프로젝트명, 생성일자,
- *        최종수정일, 프로젝트마감일, 프로젝트상태(ing/finished/discard/archived(유료)),
+ *        최종수정일, 프로젝트마감일, 프로젝트상태(created/working/finished/discard/archived(유료)),
  *        프로젝트url(uuid)
  * mapping:
  * !mapping:
@@ -27,29 +28,45 @@ public class MmomProject extends BaseEntity{
     private Long id;
 
     private Long creator;
+
     private String projectType;
     private String projectName;
-
-//    @CreatedDate
-//    @Column(updatable = false)
-//    private LocalDateTime createdAt;
-//
-//    @LastModifiedDate
-//    private LocalDateTime updatedAt;
 
     @Column(nullable = true)
     private LocalDateTime projectDueDate;
 
     @Column(nullable = true)
-    @ColumnDefault("ing")
+    @ColumnDefault("created") // for ddl // @Column(columnDefinition = "varchar(255) default 'Created'") for
     private String projectStatus;
 
-    private String projectURL;
+    @Column(unique = true)
+    private UUID projectURL;
 
-    @Builder
-    public MmomProject(Long creator, String projectType, String projectName){
-        this.creator = creator;
+    // 수정이 가능한 필드:
+    public void setProjectType(String projectType) {
         this.projectType = projectType;
+    }
+
+    public void setProjectName(String projectName) {
         this.projectName = projectName;
     }
+
+    public void setProjectDueDate(LocalDateTime projectDueDate) {
+        this.projectDueDate = projectDueDate;
+    }
+
+    public void setProjectStatus(String projectStatus) {
+        this.projectStatus = projectStatus;
+    }
+
+    @Builder
+    public MmomProject(Long creator, String type, String name, LocalDateTime dueDate, UUID url){
+        this.creator = creator;
+        this.projectType = type;
+        this.projectName = name;
+        this.projectDueDate = dueDate;
+        this.projectURL = url;
+    }
+
+
 }
